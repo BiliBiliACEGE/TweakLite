2f8f6452a43189ebc3f4ce38db2100be2db6d5d0
BiliBiliACEGE <3486598566@qq.com>
2025-11-20 19:47:38 +0800

    update

--- src/main/java/net/ace/TweakLite.java
+++ src/main/java/net/ace/TweakLite.java
@@ -5,7 +5,7 @@ import net.ace.network.NetworkRegistry;
 import net.fabricmc.api.ModInitializer;
 
 public class TweakLite implements ModInitializer {
-	public static final String MOD_ID = "TweakLite";
+	public static final String MOD_ID = "tweaklite";
 	@Override
 	public void onInitialize() {
 		NetworkRegistry.initServer();
